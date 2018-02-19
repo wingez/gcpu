e669030e973ff10a37be2507ec60612c3f7cde34
Wingez <gustav.ledous@gmail.com>
2018-02-19 12:31:06 +0100

    New interface with Click library

---
+++ gcpu/main.py
@@ -0,0 +1,35 @@
+import click
+import gcpu._version
+from gcpu.microcode import core
+from gcpu.compiler import compiler
+
+pass_verbose = click.make_pass_decorator(bool)
+
+
+@click.group(invoke_without_command=True)
+@click.option('--verbose', is_flag=True, help='Print verbose messages')
+@click.option('--version', is_flag=True)
+@click.pass_context
+def cli(ctx, verbose, version):
+    ctx.obj = verbose
+
+    if version:
+        print(gcpu._version.__version__)
+
+
+@cli.command()
+@click.option('--suppress-warnings', is_flag=True, help=' Do not print warnings during compilation')
+@click.argument('configfile', type=click.Path(exists=True))
+@click.argument('file', type=click.Path(exists=True))
+@pass_verbose
+def compile(verbose, suppress_warnings, configfile, file):
+    core.loadinstructions(configfile)
+    compiler.compile(file, 'output')
+    print('done')
+
+@cli.command()
+@click.argument('configfile', type=click.Path(exists=True))
+@pass_verbose
+def microcode(verbose, configfile):
+    core.loadinstructions(configfile)
+    print('done')
--- setup.py
+++ setup.py
@@ -1,17 +1,35 @@
-
-from setuptools import setup
-
-
+from setuptools import setup, find_packages
+import re
 
 
+def getversion():
+    VERSIONFILE = 'gcpu/_version.py'
+    verstrline = open(VERSIONFILE, 'rt').read()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {}'.format(VERSIONFILE))
 
 
-
-setup(name='gcpu',
-      version='1.0',
-      description='',
-      url='',
-      author_email='gustav.ledous@gmail.com',
-      license='MIT',
-      #packages=['funniest'],
-      zip_safe=False)
+setup(
+    name='gcpu',
+    version=getversion(),
+    description='',
+    url='',
+    author_email='gustav.ledous@gmail.com',
+    license='MIT',
+    include_package_data=True,
+    py_modules=['gcpu.main'],
+    install_requires=[
+        'Click',
+        'Jinja2',
+        'Markdown',
+    ],
+    entry_points='''
+        [console_scripts]
+        gcpu=gcpu.main:cli
+    ''',
+    zip_safe=False,
+)
