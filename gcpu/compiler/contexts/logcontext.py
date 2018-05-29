c989fece4c0fc852551f04b6c0960df35c214523
Wingez <gustav.ledous@gmail.com>
2018-05-30 01:39:05 +0200

    Better parsing of logging messages

--- gcpu/compiler/contexts/logcontext.py
+++ gcpu/compiler/contexts/logcontext.py
@@ -3,6 +3,7 @@ from gcpu.compiler import compiler
 
 logall = 'logf '
 logphase2 = 'log '
+evaldelimeter = '$'
 
 
 class LogContext(Context):
@@ -19,11 +20,24 @@ class LogContext(Context):
         elif statement.startswith(logall):
             statement = statement[len(logall):]
 
-        result = str(self.scope.evaluate(statement))
+        result = self.parsestatement(statement)  # str(self.scope.evaluate(statement))
         print('log file:{} line:{}  {}'.format(self.compiler.name,
                                                self.compiler.linenumber,
                                                result))
         self.end()
+
+    def parsestatement(self, statement: str):
+        result = statement
+        if evaldelimeter in statement:
+            result, *junk, statement = statement.partition(evaldelimeter)
+            rest = ''
+            if ' ' in statement:
+                statement, *junk, rest = statement.partition(' ')
+
+            result += str(self.scope.evaluate(statement))
+            if rest:
+                result += self.parsestatement(rest)
+        return result
 
     @classmethod
     def checkstart(cls, statement: str):
